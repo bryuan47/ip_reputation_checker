d122905431ec6824ac470d7ad578eb8a079c7a66
Bryuan Mathis <76184628+bryuan47@users.noreply.github.com>
2025-06-03 21:14:50 -0400

    Add files via upload

---
+++ IP_Reputation_Checker.py
@@ -0,0 +1,32 @@
+import requests
+
+API_KEY = "a629f69981deb50dbfbadd68fded4fe8bba59b1cf5bc71218d5579d923f144d95ce74d6c87b29731"
+ip = input ("Enter a suspected IP address: ")
+
+url = "https://api.abuseipdb.com/api/v2/check"
+
+querystring = {
+    "ipAddress": ip,
+    "maxAgeInDays": "90" 
+}
+
+headers = {
+    "Accept": "application/json",
+    "Key": API_KEY
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+if response.status_code == 200:
+    result=response.json()["data"]
+    print(f"\n[+] IP: {result["ipAddress"]}")
+    print(f"[+] Abuse Score: {result["abuseConfidenceScore"]}/100")
+    print(f"[+] Country: {result["countryCode"]}")
+    print(f"[+] Domain: {result["domain"]}")
+    print(f"[+] Total Reports: {result["totalReports"]}")
+    print(f"[+] Last Reported:{result["lastReportedAt"]}")
+
+else:
+    print("[-] Error:", response.status_code)
+
+
